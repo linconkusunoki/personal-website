92b14915b6f4b93b52a87e7f2b8e1d4a11f6175b
Lincon Kusunoki <linconkusunoki@gmail.com>
2020-08-19 14:45:35 +0200

    add darkmode

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,12 @@
+import { useEffect } from 'react'
 import { AppProps } from 'next/app'
+import Darkmode from 'darkmode-js'
 import '../styles/index.css'
 import 'prismjs/themes/prism-okaidia.css'
 
 export default function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    new Darkmode({ label: '🌓' }).showWidget()
+  }, [])
   return <Component {...pageProps} />
 }
